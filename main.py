c688253864dbd0699f5c710c5ba401fb46528e7d
cjkobs <ckobs@asu.edu>
2022-08-29 19:31:59 -0700

    Basically finished

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from scipy.optimize import minimize
 import numpy as np
 
 # We want to minimize ||f(x)-xinit|| subject to f(x) = 0
-
-f = (-4*x[0])**2 + (3*x[0]-x[1]-2)**2 + (x[1]-1)**2 + (x[0]-1)**2
-# the euclidean norm of f-xinit
-objfun = lambda x, xinit: np.sqrt(np.sum((f(x)-xinit)**2))
+fun = lambda x: (x[0]-x[1])**2 + (x[1]+x[2]-2)**2 + (x[3]-1)**2+(x[4]-1)**2
 
 # initial guess
-xinit = [1, 1]
+xinit = [10, 10, 10, 10, 10]
+# the euclidean norm of f-xinit
 # constraint f(x) = 0:
-constr = {"type": "eq", "fun": f}
-res= minimize(fun=objfun, args=(xinit,), x0=xinit, constraints=constr)
+constr = ({"type": "ineq", "fun": lambda x: x[0] + 3*x[2]},
+          {"type": "ineq", "fun": lambda x: x[2] + x[3] - 2*x[4]},
+          {"type": "ineq", "fun": lambda x: x[1] - x[4]})
+bnds = ((-10, 10), (-10, 10), (-10, 10), (-10, 10), (-10, 10))
+res = minimize(fun, xinit, method='SLSQP', bounds=bnds, constraints=constr)
 print(res.x)
